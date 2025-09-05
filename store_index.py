3da37beeec7ed1b8f225bfe042fda218f2474995
anshu57 <gangwaranshu3@gmail.com>
2025-09-05 21:50:16 +0530

    Added Nginx code for deployment 5.2

--- store_index.py
+++ store_index.py
@@ -7,23 +7,14 @@ from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
 
-
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-HUGGINGFACEHUB_API_TOKEN=os.environ.get('HUGGINGFACEHUB_API_TOKEN')
-
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
-
-
-
 extracted_data=load_pdf_files(data='data/')
 filter_data = filter_to_minimal_docs(extracted_data)
 text_chunks=text_split(filter_data)
 
 embeddings = download_hugging_face_embeddings()
 
-pinecone_api_key = PINECONE_API_KEY
-pc = Pinecone(api_key=pinecone_api_key)
+# The Pinecone client will automatically find the PINECONE_API_KEY from the environment.
+pc = Pinecone()
 
 
 
